16e5da9eb3e86cee417e94cc307b798fa5aa99d1
Ibrahim Anifowoshe <ibrahim.anifowoshe.uk@gmail.com>
2024-06-22 00:09:08 +0100

    Global Login Auth setup added

---
+++ src/tests/global/login-setup.ts
@@ -0,0 +1,19 @@
+import { MANUAL_UK } from "../../../playwright.config";
+import {test as setup} from "../../fixtures/base";
+
+setup("Login Authentication", async ({page, loginPage}) => {
+    //Get the environment variables
+    const email = process.env.EMAIL!;
+    const password = process.env.PASSWORD!;
+
+    //Navigate to the login page
+    await loginPage.navigateToLoginPage();
+
+    //Login
+    await loginPage.assertLoginHeaderVisible();
+    await loginPage.clickAcceptCookies();  
+    await loginPage.assertLoginBtnDisabled();
+    await loginPage.login(email, password);
+
+    await page.context().storageState({ path: MANUAL_UK });
+});
